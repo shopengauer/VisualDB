b6812a0919fa1f72a31c828bdc237d2be14b356b
Василий <localducks@gmail.com>
2015-07-13 21:05:48 +0300

    Commit 2

--- src/main/resources/static/angularjs/controllers/deviceListControllers.js
+++ src/main/resources/static/angularjs/controllers/deviceListControllers.js
@@ -1,3 +1,23 @@
 /**
  * Created by Vasiliy on 13.07.2015.
  */
+
+angular.module("VisualDB").constant("activeListTypeClass","btn-primary").controller("deviceListCtrl",function($scope,$filter,activeListTypeClass){
+
+    var selectedType = null;
+
+    $scope.selectType = function(newType){
+        selectedType = newType;
+    }
+
+    $scope.typeFilterFn = function(device){
+           return selectedType == null || device.devicetype == selectedType;
+
+    }
+
+
+    $scope.getTypeClass= function(type){
+        return selectedType == type ? activeListTypeClass : "";
+    }
+
+})
