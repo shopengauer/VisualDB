3e99778ea5a2aa4a558a1efa3f8051c3545778aa
Василий <localducks@gmail.com>
2015-07-16 01:04:09 +0300

    Commit 2

---
+++ src/main/java/com/vspavlov/visualdb/domain/AssemblyUnit.java
@@ -0,0 +1,17 @@
+package com.vspavlov.visualdb.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Василий on 15.07.2015.
+ */
+@Entity
+public class AssemblyUnit {
+
+
+   private String cod;
+   private String specification;
+   private String goods;
+
+
+}
